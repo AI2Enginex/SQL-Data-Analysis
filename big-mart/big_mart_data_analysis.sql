0809c4f9115331dc9bfe7258c19910234bd2d75a
Vibhor Sakalley <vibhorsakalley@gmail.com>
2024-09-30 17:41:36 +0530

    LAG and FIRST_VALUE added

--- big-mart/big_mart_data_analysis.sql
+++ big-mart/big_mart_data_analysis.sql
@@ -156,3 +156,100 @@ select
 	total_sales,
 	RANK() OVER(Order by total_sales DESC) as rank_by_item
 from AGGEGRATE_ITEM_RANK;
+
+
+---- sales difference by year
+with LaggedDataQuery AS (
+	  select 
+	   outlet_establishment_year,
+	   sum(item_outlet_sales) as total_sales
+	from 
+	   train
+	group by
+	   Outlet_Establishment_Year
+)
+select 
+	outlet_establishment_year,
+	total_sales,
+	LAG(total_sales) OVER(ORDER BY outlet_establishment_year) as previous_year_sales,
+	ROUND((total_sales - LAG(total_sales) OVER(ORDER BY outlet_establishment_year)),2) as sales_difference
+from LaggedDataQuery;
+
+---- previous year sales comparison using lag function for item 
+with LaggedDataQuery AS (
+	  select 
+	   outlet_establishment_year,
+	   item_type,
+	   sum(item_outlet_sales) as total_sales
+	from 
+	   train
+	group by
+	   Outlet_Establishment_Year,
+	   Item_Type
+)
+select
+  outlet_establishment_year,
+  item_type,
+  total_sales,
+  LAG(total_sales) OVER(ORDER BY outlet_establishment_year) as lagged_sales
+from LaggedDataQuery where Item_Type = 'Meat';
+
+----- sales difference by item and year
+with LaggedDataQuery AS (
+	  select 
+	   outlet_establishment_year,
+	   item_type,
+	   sum(item_outlet_sales) as total_sales
+	from 
+	   train
+	group by
+	   Outlet_Establishment_Year,
+	   Item_Type
+)
+select
+  outlet_establishment_year,
+  item_type,
+  total_sales,
+  LAG(total_sales) OVER(PARTITION BY Item_type ORDER BY outlet_establishment_year) as lagged_sales,
+  ROUND((total_sales - LAG(total_sales) OVER(PARTITION BY item_type ORDER BY outlet_establishment_year)),2) as sales_difference
+from LaggedDataQuery;
+
+
+---- most sold item by year
+WITH FirstValueDataQuery AS (
+    SELECT 
+        outlet_establishment_year,
+        item_type,
+        SUM(item_outlet_sales) AS total_sales
+    FROM 
+        train
+    GROUP BY
+        outlet_establishment_year,
+        item_type
+)
+SELECT
+    outlet_establishment_year,
+    item_type,
+    total_sales,
+    FIRST_VALUE(item_type) OVER (PARTITION BY outlet_establishment_year ORDER BY total_sales DESC) AS most_sold_item
+FROM FirstValueDataQuery;
+
+
+---- least sold item by year
+WITH FirstValueDataQuery AS (
+    SELECT 
+        outlet_establishment_year,
+        item_type,
+        SUM(item_outlet_sales) AS total_sales
+    FROM 
+        train
+    GROUP BY
+        outlet_establishment_year,
+        item_type
+)
+SELECT
+    outlet_establishment_year,
+    item_type,
+    total_sales,
+    FIRST_VALUE(item_type) OVER (PARTITION BY outlet_establishment_year ORDER BY total_sales ASC) AS most_sold_item
+FROM FirstValueDataQuery;
