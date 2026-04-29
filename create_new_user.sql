dd3a06117332f950997f50d89ca38e9853fe03b9
Vibhor Sakalley <vibhorsakalley@gmail.com>
2026-04-29 22:16:26 +0530

    updated queries

--- create_new_user.sql
+++ create_new_user.sql
@@ -1,6 +1,15 @@
 CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';
+
+--- grant ALL PRIVILEGES for the entire database to 'myuser'
 GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'localhost';
+
+--- grant ALL PRIVILEGES for database cars to 'myuser' 
+GRANT ALL PRIVILEGES ON cars.* TO 'myuser'@'localhost';
+
+--- grant only limited access for the bigmart_sales database to 'myuser'
+GRANT SELECT, INSERT ON bigmart_sales.* TO 'myuser'@'localhost';
 FLUSH PRIVILEGES;
+
 
 
 
